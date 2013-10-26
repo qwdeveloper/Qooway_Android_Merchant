c26a71b2ffdbf2b0a8b9ae8d8a8453b176abe243
qwdeveloper <david@qooway.com>
2013-10-25 17:50:21 -0700

    oct -25 Commt

--- merchantv01/src/com/example/merchantv01/MainActivity.java
+++ merchantv01/src/com/example/merchantv01/MainActivity.java
@@ -43,7 +43,6 @@ public class MainActivity extends Activity {
 	private DrawerLayout mDrawerLayout;
 	private ListView mDrawerList;
 	private ActionBarDrawerToggle mDrawerToggle;
-
 	private CharSequence mDrawerTitle;
 	private CharSequence mTitle;
 	private String[] mPlanetTitles;
@@ -110,77 +109,18 @@ public class MainActivity extends Activity {
 
 		    if (savedInstanceState == null) {
 		        selectItem(0);
-		        
-		        
-		        //code for testing HTTP / XML fetch
-		        ConnectivityManager connMgr = (ConnectivityManager) 
-		            getSystemService(Context.CONNECTIVITY_SERVICE);
-		        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-		        if (networkInfo != null && networkInfo.isConnected()) {
-		            new DownloadWebpageTask().execute(stringUrl);
-		        } else {
-		          //  textView.setText("No network connection available.");
-		        }
 		    }
+		    
+		       //code for testing HTTP / XML fetch
+	        ConnectivityManager connMgr = (ConnectivityManager) 
+	            getSystemService(Context.CONNECTIVITY_SERVICE);
+	        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+	        if (networkInfo != null && networkInfo.isConnected()) {
+	            new DownloadWebpageTask().execute(stringUrl);
+	        } else {
+	            displayText.setText("No network connection available.");
+	        }
 		}
-	
-	 private class DownloadWebpageTask extends AsyncTask<String, Void, String> {
-	        @Override
-	        protected String doInBackground(String... urls) {
-	              
-	            // params comes from the execute() call: params[0] is the url.
-	            try {
-	                return downloadUrl(urls[0]);
-	            } catch (IOException e) {
-	                return "Unable to retrieve web page. URL may be invalid.";
-	            }
-	        }
-	        // onPostExecute displays the results of the AsyncTask.
-	        @Override
-	        protected void onPostExecute(String result) {
-	            textView.setText(result);
-	       }
-	        
-	        private String downloadUrl(String myurl) throws IOException {
-	            InputStream is = null;
-	            // Only display the first 500 characters of the retrieved
-	            // web page content.
-	            int len = 500;
-	                
-	            try {
-	                URL url = new URL(myurl);
-	                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-	                conn.setReadTimeout(10000 /* milliseconds */);
-	                conn.setConnectTimeout(15000 /* milliseconds */);
-	                conn.setRequestMethod("GET");
-	                conn.setDoInput(true);
-	                // Starts the query
-	                conn.connect();
-	                int response = conn.getResponseCode();
-	                Log.d(DEBUG_TAG, "The response is: " + response);
-	                is = conn.getInputStream();
-
-	                // Convert the InputStream into a string
-	                String contentAsString = readIt(is, len);
-	                return contentAsString;
-	                
-	            // Makes sure that the InputStream is closed after the app is
-	            // finished using it.
-	            } finally {
-	                if (is != null) {
-	                    is.close();
-	                } 
-	            }
-	        }
-	        public String readIt(InputStream stream, int len) throws IOException, UnsupportedEncodingException {
-	            Reader reader = null;
-	            reader = new InputStreamReader(stream, "UTF-8");        
-	            char[] buffer = new char[len];
-	            reader.read(buffer);
-	            return new String(buffer);
-	        }
-	
-	 }
 	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -280,6 +220,65 @@ public void onConfigurationChanged(Configuration newConfig) {
 /**
  * Fragment that appears in the "content_frame", shows a planet
  */
+private class DownloadWebpageTask extends AsyncTask<String, Void, String> {
+    @Override
+    protected String doInBackground(String... urls) {
+          
+        // params comes from the execute() call: params[0] is the url.
+        try {
+            return downloadUrl(urls[0]);
+        } catch (IOException e) {
+            return "Unable to retrieve web page. URL may be invalid.";
+        }
+    }
+    // onPostExecute displays the results of the AsyncTask.
+    @Override
+    protected void onPostExecute(String result) {
+        displayText.setText(result);
+   }
+    
+    private String downloadUrl(String myurl) throws IOException {
+        InputStream is = null;
+        // Only display the first 500 characters of the retrieved
+        // web page content.
+        int len = 500;
+            
+        try {
+            URL url = new URL(myurl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            // Starts the query
+            conn.connect();
+            int response = conn.getResponseCode();
+            String DEBUG_TAG = "YOUR_APP_NAME";
+            Log.d(DEBUG_TAG, "The response is: " + response);
+            is = conn.getInputStream();
+
+            // Convert the InputStream into a string
+            String contentAsString = readIt(is, len);
+            return contentAsString;
+            
+        // Makes sure that the InputStream is closed after the app is
+        // finished using it.
+        } finally {
+            if (is != null) {
+                is.close();
+            } 
+        }
+    }
+    public String readIt(InputStream stream, int len) throws IOException, UnsupportedEncodingException {
+        Reader reader = null;
+        reader = new InputStreamReader(stream, "UTF-8");        
+        char[] buffer = new char[len];
+        reader.read(buffer);
+        return new String(buffer);
+    }
+
+}
+
 public static class PageFragment extends Fragment {
     public static final String ARG_PLANET_NUMBER = "planet_number";
 
@@ -298,10 +297,7 @@ public static class PageFragment extends Fragment {
         	switch (name) {
             case RECORDTRANSACTIONS:  return inflater.inflate(R.layout.fragment_record, container, false);
             case MISSINGTRANSACTIONS:  return inflater.inflate(R.layout.fragment_missing, container, false);
-
             default: return inflater.inflate(R.layout.fragment_sales, container, false);
-            
-            
         	}
 }
 }
